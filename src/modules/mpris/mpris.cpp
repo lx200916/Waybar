b394d0019fa2839f53655040566f743034d23e72
chayleaf <chayleaf-git@pavluk.org>
2023-02-17 05:34:43 +0700

    mpris: account for unicode's East_Asian_Width property

--- src/modules/mpris/mpris.cpp
+++ src/modules/mpris/mpris.cpp
@@ -187,13 +187,30 @@ auto Mpris::getIcon(const Json::Value& icons, const std::string& key) -> std::st
   return "";
 }
 
-// because abruptly cut titles look weird
+#include "wide_check.h"
 
-// adapted from https://stackoverflow.com/questions/30995246/substring-of-a-stdstring-in-utf-8-c11
+// Japanese characters, emoji, etc are twice as long in mono fonts
+auto utf8_char_wide(const std::string& str, size_t pos) -> bool {
+  uint32_t c = (unsigned char)str[pos];
+  if (c <= 127) return false;
+  else if ((c & 0xE0) == 0xC0) {
+    if (pos + 1 >= str.length()) return false;
+    c = (str[pos + 1] & 0x3F) | ((c & 0x1F) << 6);
+  } else if ((c & 0xF0) == 0xE0) {
+    if (pos + 2 >= str.length()) return false;
+    c = (str[pos + 2] & 0x3F) | ((str[pos + 1] & 0x3F) << 6) | ((c & 0x1F) << 12);
+  } else if ((c & 0xF8) == 0xF0) {
+    if (pos + 3 >= str.length()) return false;
+    c = (str[pos + 3] & 0x3F) | ((str[pos + 2] & 0x3F) << 6) | ((str[pos + 1] & 0x3F) << 12) | ((c & 0x1F) << 18);
+  }
+  return char_is_wide(c);
+}
+
 auto utf8_length(const std::string& str) -> size_t {
   size_t i = 0, len = 0;
-  for (i = len = 0; i < str.length(); i++, len++) {
+  for (i = len = 0; i < str.length(); i++) {
     unsigned char c = (unsigned char)str[i];
+    len += utf8_char_wide(str, i) ? 2 : 1;
     if (c <= 127)
       continue;
     else if ((c & 0xE0) == 0xC0)
@@ -202,15 +219,12 @@ auto utf8_length(const std::string& str) -> size_t {
       i += 2;
     else if ((c & 0xF8) == 0xF0)
       i += 3;
-    else if ((c & 0xFC) == 0xF8)
-      i += 4;
-    else if ((c & 0xFE) == 0xFC)
-      i += 5;
     else
       return str.length();  // invalid utf8
   }
   return len;
 }
+
 std::string utf8_truncate(const std::string& str, size_t len) {
   if (len == 0) {
     return std::string();
@@ -220,12 +234,13 @@ std::string utf8_truncate(const std::string& str, size_t len) {
   }
   size_t pos = std::string::npos;
   size_t i = 0, u = 0;
-  for (i = u = 0; i < str.length(); i++, u++) {
+  for (i = u = 0; i < str.length(); i++) {
     if (u <= len) {
       pos = i;
     } else {
       break;
     }
+    u += utf8_char_wide(str, i) ? 2 : 1;
     unsigned char c = (unsigned char)str[i];
     if (c <= 127)
       continue;
@@ -235,10 +250,6 @@ std::string utf8_truncate(const std::string& str, size_t len) {
       i += 2;
     else if ((c & 0xF8) == 0xF0)
       i += 3;
-    else if ((c & 0xFC) == 0xF8)
-      i += 4;
-    else if ((c & 0xFE) == 0xFC)
-      i += 5;
     else
       return str.substr(0, len);  // invalid utf8
   }
